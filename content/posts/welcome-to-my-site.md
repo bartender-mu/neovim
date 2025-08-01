695f57922c581e66f1c0349f46a8576681a08082
Thiery Louison <bartender.mu@gmail.com>
2025-08-01 14:15:08 +0400

    update telescope

--- content/posts/welcome-to-my-site.md
+++
@@ -1,6 +0,0 @@
----
-title: "Welcome to My Site"
-date: 2025-07-10
-draft: false
----
-This is a Hugo-powered site for the bartender-mu/neovim project!
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,10 +1,38 @@
 return {
   'nvim-telescope/telescope.nvim',
-  tag = '0.1.5',
-  dependencies = { 'nvim-lua/plenary.nvim' },
+  tag = '0.1.8',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-lua/popup.nvim',
+    { 'nvim-telescope/telescope-media-files.nvim' },
+  },
   config = function()
-    vim.keymap.set('n', '<leader>ff', ':Telescope find_files<CR>', { noremap = true }) -- <Space>ff
-    vim.keymap.set('n', '<leader>fg', ':Telescope live_grep<CR>', { noremap = true }) -- <Space>fg
-    vim.keymap.set('n', '<leader>fb', ':Telescope buffers<CR>', { noremap = true }) -- <Space>fb
+    require('telescope').setup {
+      defaults = {
+        layout_strategy = 'horizontal',
+        layout_config = {
+          horizontal = {
+            prompt_position = 'top',
+            width = { padding = 0 },
+            height = { padding = 0 },
+            preview_width = 0.5,
+          },
+        },
+        sorting_strategy = 'ascending',
+        mappings = {
+          i = {
+            ['<C-h>'] = 'which_key',
+          },
+        },
+      },
+      extensions = {
+        media_files = {
+          filetypes = { 'png', 'jpg', 'jpeg', 'gif', 'pdf' },
+          find_cmd = 'fd',
+        },
+      },
+    }
+    require('telescope').load_extension('media_files')
+    vim.keymap.set('n', '<Leader>fm', ':Telescope media_files<CR>', { noremap = true, silent = true, desc = 'Media Files' })
   end,
 }
